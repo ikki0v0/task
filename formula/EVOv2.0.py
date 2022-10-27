e48fedbc20415633068cbd549cc0b7b4a84ec548
ikki0v0 <1014733014@qq.com>
2022-10-27 10:43:18 +0800

    10.27

---
+++ formula/EVOv2.0.py
@@ -0,0 +1,108 @@
+# 把excel的数字复制进BUII并计算（左右眼无差异）
+# 不要debug，会卡死
+import os
+import time
+from selenium import webdriver
+from chardet import detect
+from selenium.webdriver.common.keys import Keys
+import pandas as pd
+from tqdm import tqdm
+
+path = 'D:\\Program Files\\chromedriver.exe'  # chromedriver.exe不在python安装目录，则需要手动设置位置
+data_root = "F:\\deep learning\\other\\公式计算"
+data_path = os.path.join(data_root, "CTR补.csv")  # 病人参数读取路径
+web_path = os.path.join(data_root, "web_xpath.csv")
+
+
+# 定义预处理操作
+def change_encode(file_path):
+    """转码：路径列表里的csv文件批量转换为utf-8编码格式"""
+    with open(file_path, 'rb+') as fp:
+        content = fp.read()
+        encoding = detect(content)['encoding']
+        content = content.decode(encoding).encode('utf_8_sig')
+        fp.seek(0)
+        fp.write(content)
+
+
+def init_info():
+    """填写初始无关信息"""
+    driver.find_element_by_xpath(xpath.loc['surgeon', f]).send_keys(1)  # Doctor name
+    driver.find_element_by_xpath(xpath.loc['patient', f]).send_keys(1)  # Patient name
+    driver.find_element_by_xpath(xpath.loc['patientid', f]).send_keys(1)  # Patient ID
+
+
+def fill_parameters(series):
+    """填写眼部生物学参数，传入series"""
+    driver.find_element_by_xpath(xpath.loc['constant', f]).send_keys(str(series['constant']))  # 以字符串格式输入！！
+
+    driver.find_element_by_xpath(xpath.loc['AL', f]).send_keys(str(series['AL']))  # AL
+    driver.find_element_by_xpath(xpath.loc['K1', f]).send_keys(str(series['K1']))  # K1
+    driver.find_element_by_xpath(xpath.loc['K2', f]).send_keys(str(series['K2']))  # K2
+    driver.find_element_by_xpath(xpath.loc['ACD', f]).send_keys(str(series['ACD']))  # ACD
+
+    driver.find_element_by_xpath(xpath.loc['LT', f]).send_keys(str(series['LT']))  # LT
+    driver.find_element_by_xpath(xpath.loc['CCT', f]).send_keys(str(series['CCT']))  # CCT
+
+
+def get_result():
+    """点击计算并切换到结果界面，获取IOL度数和对应的refraction"""
+    driver.find_element_by_xpath('//*[@id="btnCalculate"]').click()  # 点击计算
+    # time.sleep(1)
+    # 判断power是否在列表里
+    d = driver.find_element_by_xpath('//*[@id="PnPred"]/table/tbody').text
+    list = d.split()
+    list = list[34:44]
+    return list
+
+
+# 读取excel
+change_encode(data_path)
+df = pd.read_csv(data_path, header=0, encoding="utf-8")
+# 读取对应公式网站的xpath
+f = 'EVO'  # 确定计算的公式
+xpath = pd.read_csv(web_path, header=0, index_col=0, encoding="utf-8")
+
+
+# 打开网页
+driver = webdriver.Chrome(executable_path=path)
+url = "https://www.evoiolcalculator.com/calculator.aspx"
+driver.get(url)
+
+# 遍历数据框
+for i in tqdm(range(df.shape[0])):
+    series = df.iloc[i]
+    val = 0  # 用与调整结果的晶体度数范围
+
+    # 填写信息
+    init_info()   # 初始病人信息填写
+    fill_parameters(series)  # 把数据输进网页
+    list = get_result()  # 计算并得到结果列表
+
+    power = series['power']
+    if power%1 == 0:   # 如果是整数，转换为int类型，否则barret会对不上
+        power = int(power)
+
+    # 不断调整，直到结果列表里有对应的晶体power
+    while power > float(list[len(list)-2]):
+        val = val - 1
+        driver.find_element_by_xpath('//*[@id="btnBack"]').click()  # 返回填写页面
+        driver.find_element_by_xpath('//*[@id="txtRefraction"]').clear()
+        driver.find_element_by_xpath('//*[@id="txtRefraction"]').send_keys(val)  # Refraction
+        # 计算
+        list = get_result()
+
+    if str(power) in list:  # 记得索引字符串！！
+        refraction = list[list.index(str(power)) + 1]
+
+    df.loc[i, 'EVO'] = refraction
+    print("\n", df.iloc[i, :])
+
+    # 初始化回原来的界面
+    driver.find_element_by_xpath('//*[@id="btnBack"]').click()  # 返回填写页面
+    time.sleep(1)
+    driver.find_element_by_xpath('//*[@id="btnReset"]').click()  # 清空页面
+
+df.to_csv(os.path.join(data_root, 'result.csv'), header=True, encoding='utf_8_sig')
+print("计算完成！")
+
